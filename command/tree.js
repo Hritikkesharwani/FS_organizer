af27abbf72b66353c8de1d2545b219fa822b2c8f
HritikKesharwani <sanjukesharwani2000@gmail.com>
2021-07-25 16:41:50 +0530

    FS orgnizer completed

--- command/tree.js
+++ command/tree.js
@@ -1,5 +1,11 @@
-function fn (){
-    console.log("Tree command executed with ");
+let fs = require("fs");
+
+
+function fn (path){
+    let Allfiles = fs.readdirSync(path);
+    for(let i=0;i<Allfiles.length;i++){
+        console.log(Allfiles[i]);
+    }
     
 
 }
